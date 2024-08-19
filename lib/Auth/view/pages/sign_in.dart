433c0fdbe513cffad8dbf0d1a19b7c1261365790
ammar <ammaralshalet@gmail.com>
2024-08-19 15:01:31 +0300

    Make the Response login responsive

--- lib/Auth/view/pages/sign_in.dart
+++ lib/Auth/view/pages/sign_in.dart
@@ -1,8 +1,9 @@
+import 'package:app_with_team/responsive/responsive.dart';
+import 'package:flutter/material.dart';
 import 'package:app_with_team/Auth/res/strings.dart';
 import 'package:app_with_team/Auth/view/pages/sign_up_screen.dart';
 import 'package:app_with_team/Auth/view/widget/Buttons.dart';
 import 'package:app_with_team/Auth/view/widget/TextFields.dart';
-import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -13,12 +14,13 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final TextEditingController emailController = TextEditingController();
-  final TextEditingController confirmPasswordController =
-      TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   bool obscurePassword = true;
+
   @override
   Widget build(BuildContext context) {
+    Responsive responsive = Responsive(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -39,11 +41,14 @@ class _SignInState extends State<SignIn> {
           },
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Container(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(
+            responsive.getWidthPercentage(5),
+          ),
+          child: Column(
+            children: [
+              Container(
                 alignment: Alignment.bottomLeft,
                 child: const Text(
                   textSignIn,
@@ -51,125 +56,132 @@ class _SignInState extends State<SignIn> {
                     fontSize: 24,
                     fontWeight: FontWeight.w500,
                   ),
-                )),
-            const SizedBox(height: 20),
-            CustomTextField2(
-              labelText: 'Email or Phone Number',
-              controller: emailController,
-            ),
-            const SizedBox(height: 20),
-            CustomPasswordField(
-              labelText: 'Enter Your Password',
-              controller: passwordController,
-              obscureText: obscurePassword,
-              onChanged: (password) {},
-              toggleObscureText: () {
-                setState(() {
-                  obscurePassword = !obscurePassword;
-                });
-              },
-            ),
-            Container(
-              alignment: Alignment.bottomRight,
-              child: TextButton(
-                onPressed: () {},
-                child: const Text(
-                  'Forget password?',
-                  style: TextStyle(
-                    color: Color.fromRGBO(243, 1, 1, 1),
+                ),
+              ),
+              SizedBox(
+                height: responsive.getHeightPercentage(3),
+              ),
+              CustomTextField2(
+                labelText: 'Email or Phone Number',
+                controller: emailController,
+              ),
+              SizedBox(height: responsive.getHeightPercentage(3)),
+              CustomPasswordField(
+                labelText: 'Enter Your Password',
+                controller: passwordController,
+                obscureText: obscurePassword,
+                onChanged: (password) {},
+                toggleObscureText: () {
+                  setState(
+                    () {
+                      obscurePassword = !obscurePassword;
+                    },
+                  );
+                },
+              ),
+              Container(
+                alignment: Alignment.bottomRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Forget password?',
+                    style: TextStyle(
+                      color: Color.fromRGBO(243, 1, 1, 1),
+                    ),
                   ),
                 ),
               ),
-            ),
-            MainButton(
+              SizedBox(
+                height: responsive.getHeightPercentage(3),
+              ),
+              MainButton(
                 textTheButton: 'Sign Up',
-                onTap: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //     builder: (context) => ,
-                  //   ),
-                  // );
-                }),
-            const SizedBox(height: 10),
-            Row(
-              children: const [
-                Expanded(
-                  child: Divider(
-                    thickness: 1,
-                    color: Colors.grey,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Text(
-                    'or',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Divider(
-                    thickness: 1,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            SocialLoginButton(
-              text: 'Sign up with Gmail',
-              onTap: () {},
-              image: 'assets/Gmail.png',
-            ),
-            SocialLoginButton(
-              text: 'Sign up with Facebook',
-              onTap: () {},
-              image: 'assets/Facebook.png',
-            ),
-            SocialLoginButton(
-              text: 'Sign up with Apple',
-              onTap: () {},
-              image: 'assets/Apple.png',
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
+                onTap: () {},
+              ),
+              SizedBox(height: responsive.getHeightPercentage(2)),
+              const Row(
                 children: [
-                  const Text(
-                    haveAccount,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Colors.grey,
                     ),
                   ),
-                  MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const SignUpScreen(),
-                          ),
-                        );
-                      },
-                      child: const Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          color: Color.fromRGBO(0, 137, 85, 1),
-                          fontWeight: FontWeight.bold,
-                        ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      'or',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
                       ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Colors.grey,
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
+              SizedBox(
+                height: responsive.getHeightPercentage(2),
+              ),
+              SocialLoginButton(
+                text: signGmail,
+                onTap: () {},
+                image: 'assets/Gmail.png',
+              ),
+              SocialLoginButton(
+                text: signFacebook,
+                onTap: () {},
+                image: 'assets/Facebook.png',
+              ),
+              SocialLoginButton(
+                text: signApple,
+                onTap: () {},
+                image: 'assets/Apple.png',
+              ),
+              SizedBox(
+                height: responsive.getHeightPercentage(3),
+              ),
+              Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      haveAccount,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SignUpScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            color: Color.fromRGBO(0, 137, 85, 1),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
