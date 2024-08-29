5f58df8a43634f1f3358c1f330b82caf1cb39f85
ammar <ammaralshalet@gmail.com>
2024-08-29 05:03:34 +0300

    edit on AuthBloc to save token again

--- lib/Auth/bloc/auth_bloc.dart
+++ lib/Auth/bloc/auth_bloc.dart
@@ -3,6 +3,7 @@ import 'package:app_with_team/Auth/bloc/auth_state.dart';
 import 'package:app_with_team/service/auth_service.dart';
 import 'package:app_with_team/data/models/user/handle_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final AuthService authService;
@@ -14,6 +15,9 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         ResultUserModel result = await authService.signup(event.userModel);
 
         if (result is DataSuccess) {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          await prefs.setString('token', result.token);
+
           emit(AuthSuccess());
         } else if (result is ErrorData) {
           emit(AuthFailure(result.message));
@@ -28,7 +32,19 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     on<LoginRequested>((event, emit) async {
       emit(AuthLoading());
       try {
-        emit(AuthSuccess());
+        ResultUserModel result =
+            await authService.login(event.username, event.password);
+
+        if (result is DataSuccess) {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          await prefs.setString('token', result.token);
+
+          emit(AuthSuccess());
+        } else if (result is ErrorData) {
+          emit(AuthFailure(result.message));
+        } else {
+          emit(const AuthFailure('An unknown error occurred'));
+        }
       } catch (e) {
         emit(AuthFailure('Login failed: ${e.toString()}'));
       }
